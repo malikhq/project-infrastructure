d8a2f2d52584d4b77a1b7a8d19c8e68d485776e7
abdulmalik <abdulmalik.ibikunle@gmail.com>
2026-04-14 13:38:37 +0100

    feat: update the module

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -12,8 +12,3 @@ output "cluster_name" {
   description = "The name of the EKS cluster"
   value       = module.eks.cluster_name
 }
-
-output "addon_name" {
-  description = "The name of the CloudWatch addon installed on the EKS cluster"
-  value       = module.cloudwatch.addon_name
-}
