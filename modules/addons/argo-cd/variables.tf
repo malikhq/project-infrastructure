b5248885186ca46020cea7f50f435ae7608c430e
abdulmalik <abdulmalik.ibikunle@gmail.com>
2026-04-16 15:42:21 +0100

    feat: Argocd addon added to the configuration

---
+++ modules/addons/argo-cd/variables.tf
@@ -0,0 +1,11 @@
+variable "version" {
+  description = "The version of Argo CD to install"
+  type        = string
+  default     = "4.5.2"
+}
+
+variable "namespace" {
+  description = "The namespace where Argo CD will be installed"
+  type        = string
+  default     = "argocd"
+}
--- modules/addons/cloud-watch/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "addon_name" {
-  description = "The name of the CloudWatch addon installed on the EKS cluster"
-  value       = aws_eks_addon.cloudwatch.addon_name
-}
